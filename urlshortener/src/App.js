c4dd3594a667305b58f2500014f852c0b1157a23
Chris <christophersstjohn@gmail.com>
2018-01-24 20:42:22 -0800

    Styling App.js

--- urlshortener/src/App.js
+++ urlshortener/src/App.js
@@ -1,20 +1,25 @@
 import React, { Component } from 'react';
 import LinkList from './components/LinkList';
 import CreateShortLink from './components/CreateShortLink';
+import './App.css';
+
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <div>
-                    <h2>All links</h2>
-                    <LinkList />
-                </div>
-                <div>
+          <div className="App--link-content-container">
+            <div className="App--link-content">
+                <div className="App--link-create-link">
                     <h2>Create a short link</h2>
                     <CreateShortLink />
                 </div>
+                <div className="App--link-list-links">
+                    <h2>All links</h2>
+                    <LinkList />
+                </div>
+
             </div>
+          </div>
         );
     }
 }
